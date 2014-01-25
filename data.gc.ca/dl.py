ad7c357f0c0fcadf3ab69a87ac4dee5cf0114351
Ken Pu <kenpuca@gmail.com>
2014-01-25 11:45:36 -0500

    dl.py

--- data.gc.ca/dl.py
+++ data.gc.ca/dl.py
@@ -41,20 +41,23 @@ def downloadDatasets(url):
         dataset, url2 = resolveUrl(url, href)
         if dataset:
             save(dataset, url2)
-        else: # download an index page of a few data sets
-            downloadCsvset(url2)
+        else: 
+            # download an index page of a few data sets
+            # downloadCsvset(url2)
+            pass
             
 def downloadCsvset(url):
     r = requests.get(url)
-    soup = BeautifulSoup(r.text)
-    for a in soup.find_all("a", href=lambda x: x.endswith(".csv")):
-        href = a.get('href')
-        dataset, url2 = resolveUrl(url, href)
-        save(dataset, url2)
+    if r:
+        soup = BeautifulSoup(r.text.encode("utf", "ignore"))
+        for a in soup.find_all("a", href=lambda x: x.endswith(".csv")):
+            href = a.get('href')
+            dataset, url2 = resolveUrl(url, href)
+            save(dataset, url2)
 
 def save(dataset, url):
     if dataset:
-        print "[%s] %s" % (dataset, url)
+        print "\t[%s] %s" % (dataset, url)
         with open("download/%s" % dataset, "w") as out:
             r = requests.get(url)
             if r.text:
@@ -71,7 +74,15 @@ with open(index_file, "r") as index:
     n = len(prefix)
 
     for line in index.xreadlines():
+        if line.startswith("#"): continue
         path = line.strip()
         url = SITE + path
-        downloadDatasets(url)
-        break
+        print url
+        start = time()
+        try:
+            downloadDatasets(url)
+        except:
+            pass
+        sleep(1.0)
+        t = time() - start
+        print "Total estimated download time = %.2f hours" % (t*totalDatasets / 3600)
